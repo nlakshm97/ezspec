21189e1db78e1270462287ae7ff964e4d507ee5f
nlakshm7 <nlakshm97@gmail.com>
2023-07-09 18:05:54 -0400

    text-diff

--- src/pages/Section.js
+++ src/pages/Section.js
@@ -1,7 +1,62 @@
-import './styles/Section.css'
+import './styles/Section.css';
+import React, {  useEffect } from 'react';
+
 const Section = () =>{
+
+
+    var  actual= "";
+    let diff = [];
+    useEffect(() => {
+        let textArea = document.getElementById("contentId");
+        textArea.addEventListener('input', handleChange);
+        actual = textArea.innerText;
+      }, []);
+
+    const highlightTextArea = () => {
+        console.log("highlight text area ...");
+
+    }
+
+    const handleChange = (event) =>{
+        console.log("handle change called ...");
+
+
+        let current = event.target.innerText;
+        let n = Math.min(actual.length, current.length);
+
+        let isDiffDetected = false;
+
+        for (let i= 0; i< n; i++){
+            if(actual[i] != current[i]){
+                diff.push([i, event.inputType, event.data]);
+                isDiffDetected = true;
+                break
+            }
+        }
+
+        if(! isDiffDetected){
+           diff.push([current.length - 1, "insertText", current[current.length-1]]); 
+        }
+        actual = current;
+        console.log(diff);
+    }
+
+
     return (
-        <div className='wrapper'>Hello World</div>
+        <div className='container'>
+            <div className='section'>
+                <div className='details'>
+
+                </div>
+                <div className='document'>
+                    <div className='page'>
+                        <div contentEditable='true' id="contentId" oninput="handleChange(event)">
+                            Hello world
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Section;
