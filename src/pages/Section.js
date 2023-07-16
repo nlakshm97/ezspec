375d47c00e3b171ace97ed58269e2831101ab224
nlakshm7 <nlakshm97@gmail.com>
2023-07-16 14:55:07 -0400

    improved redability

--- src/pages/Section.js
+++ src/pages/Section.js
@@ -1,97 +1,28 @@
 import './styles/Section.css';
 import React, {  useEffect } from 'react';
-import LCS from '../utility/lcs';
-import Caret from '../utility/caret';
+
+import TextDiff from '../utility/textdiff';
 
 const Section = () =>{
-
-
-    var  actualText= "";
-    let lcs = new LCS();
-    let caret = new Caret();
-
+    let textDiff = new TextDiff();
     useEffect(() => {
+        
         let textArea = document.getElementById("contentId");
-        textArea.addEventListener('input', handleChange);
+        textArea.addEventListener('input',handleChange);
         textArea.addEventListener("paste",handlePaste);
-        actualText = textArea.innerText;
+        textDiff.setActualText(textArea.innerText);
       }, []);
 
   
+      const handleChange = (event)=> {
+        textDiff.handleChange(event);
+      }
+
+      const handlePaste = (event) => {
+        textDiff.handlePaste(event);
+      }
 
 
-
-
-function handlePaste(event) {
-    console.log("handle paste called ....");
-
-
-
-    event.preventDefault();
-
-    let editableDiv = event.currentTarget;
-    let paste = (event.clipboardData || window.clipboardData).getData("text");
-    const selection = window.getSelection();
-    console.log(selection);
-    if (!selection.rangeCount) return;
-    selection.deleteFromDocument();
-
-    let spanElement = document.createElement("span");
-    spanElement.appendChild(document.createTextNode(paste));
-    selection.getRangeAt(0).insertNode(spanElement);
-    selection.collapseToEnd();
-
-
-    let caretPosition = caret.getCaretPositionDuringPaste(editableDiv, spanElement);
-    console.log(caretPosition);
-    highlightText(editableDiv);
-    caret.setCaret(editableDiv, caretPosition);
-  
-    
-}
-
-    const getCurrentText = (editableDiv) => {
-        let text = "";
-        let children = editableDiv.children;
-        for (let i=0; i<children.length; i++){
-            let child = children[i];
-            text += child.innerText;
-        }
-        console.log(text);
-        return text;
-    }
-
-    const isEditableDivEmpty = (editableDiv) => {
-        return false;
-    }
-
-    const highlightText = (editableDiv) => {
-        let currentText = getCurrentText(editableDiv);
-        console.log("current text ...", currentText);
-        let mat = lcs.findLCS(actualText, currentText);
-        let commonText = lcs.findCommonText(actualText, currentText, mat);
-        lcs.highlightText(document.getElementById("contentId"), commonText,  currentText);
-    }
-
-    const handleChange = (event) =>{
-        console.log("handle change called ...");
-
-        if (event.inputType == "insertFromPaste"){
-            return;
-        }
-        let targetId = event.target.id;
-        let editableDiv = document.getElementById(targetId);
-
-        if(isEditableDivEmpty()){
-            return;
-        }
-
-        let caretPosition = caret.getCaretPosition(editableDiv);
-        console.log(caretPosition);
-        highlightText(editableDiv);
-        caret.setCaret(editableDiv, caretPosition);
-
-    }
 
 
     return (
---
+++ src/utility/textdiff.js
@@ -0,0 +1,77 @@
+import LCS from '../utility/lcs';
+import Caret from '../utility/caret';
+export default class TextDiff{
+
+    constructor(){
+        this.lcs = new LCS();
+        this.caret = new Caret();
+    }
+
+    setActualText(actualText){
+        this.actualText = actualText;
+    }
+
+    handlePaste(event) {
+        console.log("handle paste called ....");
+
+
+
+        event.preventDefault();
+
+        let editableDiv = event.currentTarget;
+        let paste = (event.clipboardData || window.clipboardData).getData("text");
+        const selection = window.getSelection();
+        if (!selection.rangeCount) return;
+        selection.deleteFromDocument();
+
+        let spanElement = document.createElement("span");
+        spanElement.appendChild(document.createTextNode(paste));
+        selection.getRangeAt(0).insertNode(spanElement);
+        selection.collapseToEnd();
+
+
+        let caretPosition = this.caret.getCaretPositionDuringPaste(editableDiv, spanElement);
+        console.log(caretPosition);
+        this.highlightText(editableDiv);
+        this.caret.setCaret(editableDiv, caretPosition);
+    
+        
+    }
+
+    getCurrentText(editableDiv){
+        let text = "";
+        let children = editableDiv.children;
+        for (let i=0; i<children.length; i++){
+            let child = children[i];
+            text += child.innerText;
+        }
+        console.log(text);
+        return text;
+    }
+
+
+    highlightText(editableDiv){
+        let currentText = this.getCurrentText(editableDiv);
+        console.log("current text ...", currentText);
+        let mat = this.lcs.findLCS(this.actualText, currentText);
+        let commonText = this.lcs.findCommonText(this.actualText, currentText, mat);
+        this.lcs.highlightText(document.getElementById("contentId"), commonText,  currentText);
+    }
+
+    handleChange(event){
+        console.log("handle change called ...");
+     
+        if (event.inputType == "insertFromPaste"){
+            return;
+        }
+        let targetId = event.target.id;
+        let editableDiv = document.getElementById(targetId);
+
+         
+        let caretPosition = this.caret.getCaretPosition(editableDiv);
+        console.log(caretPosition);
+        this.highlightText(editableDiv);
+        this.caret.setCaret(editableDiv, caretPosition);
+
+    }
+}
